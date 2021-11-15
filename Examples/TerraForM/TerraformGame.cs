a86aed704ef40d5e1fd726391d4d4ea8c19f5d7e
Sten Marius <sten.marius.holmen@gmail.com>
2021-11-15 21:49:12 +0100

    Remove commented code

--- Examples/TerraForM/TerraformGame.cs
+++ Examples/TerraForM/TerraformGame.cs
@@ -4,30 +4,14 @@ using TerraForM.Scenes;
 
 namespace TerraForM
 {
-    public enum GameStates
+    public sealed class TerraformGame : GameBase
     {
-        InputName,
-        Menu,
-        Playing,
-        GameOver
-    }
-
-    public class TerraformGame : GameBase
-    {
-        // public Rover Rover;
-        // public Hud Hud;
         public Camera Camera { get; private set; }
-        // public Queue<Command> _currentCommands = new();
-        // public World World;
         public string Playername;
         public int Score;
         public int CurrentMap = 0;
         public bool GameOver;
         public bool IsDebugMode;
-        
-        // public GameStates GameState = GameStates.Menu;
-        // public readonly List<PlantEmitter> PlantEmitters = new();
-
         public string[] Maps = {
             // "Assets/Maps/map0.txt",
             "Assets/Maps/map1.txt",
@@ -48,121 +32,15 @@ namespace TerraForM
         protected override void OnInitialize()
         {
             Scenes.Set<InputNameScene>();
-            
-            // GameState = GameStates.InputName;
             Camera = new Camera(this);
-            // StartNewGame();
-            // Hud = new Hud(this);
-            // Camera.Follow(Rover);
         }
-
-        // private void StartNewGame()
-        // {
-        //     GameOver = false;
-        //     Score = 0;
-        //     Camera.Position = Vector2.Zero;
-        //     World = WorldLoader.LoadWorld(this, Maps[CurrentMap]);
-        //     Rover = new Rover(this) 
-        //     {
-        //         Position = World.StartingPoint.Position,
-        //         Velocity = Vector2.Zero
-        //     };
-        //     Camera.Follow(Rover);
-        //     PlantEmitters.Clear();
-        // }
 
         protected override void OnUpdate()
         {
-            // switch (GameState)
-            // {
-            //     case GameStates.InputName:
-            //         HandleNameInput();
-            //         break;
-            //     case GameStates.Menu:
-            //         if (Input.Instance.GetKey(Key.H).Pressed) GameState = GameStates.Playing;
-            //         break;
-            //     case GameStates.Playing:
-            //         if (Input.Instance.GetKey(Key.F1).Pressed) IsDebugMode = !IsDebugMode;
-            //
-            //         if (Rover.PowerDepleted() || Rover.RemainingSequences < 0)
-            //         {
-            //             GameOver = true;
-            //         }
-            //
-            //         Hud.OnUpdate();
-            //
-            //         if (Input.Instance.GetKey(Key.ESCAPE).Held) GameState = GameStates.Menu;
-            //
-            //         if (IsDebugMode)
-            //         {
-            //             if (Input.Instance.GetKey(Key.A).Held) Rover.MoveWest();
-            //             if (Input.Instance.GetKey(Key.D).Held) Rover.MoveEast();
-            //             if (Input.Instance.GetKey(Key.W).Held) Rover.MoveNorth();
-            //             if (Input.Instance.GetKey(Key.S).Held) Rover.MoveSouth();
-            //         }
-            //        
-            //         if (Input.Instance.GetKey(Key.R).Pressed) StartNewGame();
-            //
-            //         if (Input.Instance.GetKey(Key.SPACE).Pressed && !_currentCommands.Any())
-            //         {
-            //             var commands = Hud.GetCommands();
-            //             if (commands.Any())
-            //             {
-            //                 Rover.RemainingSequences--;
-            //             }
-            //             foreach (var command in commands)
-            //                 _currentCommands.Enqueue(command);
-            //         }
-            //
-            //         if (_currentCommands.Any())
-            //         {
-            //             var currentCommand = _currentCommands.Peek();
-            //             currentCommand.Update(Rover);
-            //             if (currentCommand.IsDone())
-            //             {
-            //                 _currentCommands.Dequeue();
-            //             }
-            //         }
-            //         Rover.Update();
-            //         PlantEmitters.ForEach(e => e.Update());
-            //
-            //         break;
-            //     case GameStates.GameOver:
-            //         break;
-            //     default:
-            //         break;
-            // }
-            //
-            
             Camera.Update();
         }
 
-        // private void HandleNameInput()
-        // {
-        //     if (Input.Instance.GetKey(Key.ENTER).Pressed)  GameState = GameStates.Menu;
-        //     var pressedSpace09AZkeys = Input.Instance.GetPressedKeyCodesSpace09AZ().Select(kc => (char)kc);
-        //     if (pressedSpace09AZkeys?.Any() == true)
-        //         Playername += string.Concat(pressedSpace09AZkeys);
-        //     if (Input.Instance.GetKey(Key.BACKSPACE).Pressed && !string.IsNullOrEmpty(Playername))
-        //         Playername = Playername.Substring(Playername.Length - 1);
-        // }
-
-        protected override void OnRender()
-        {
-            // World.Draw();
-            // if (GameState == GameStates.Playing)
-            // {
-            //     PlantEmitters.ForEach(e => e.Draw());
-            //     Rover.Draw();
-            //     Hud.Draw();
-            //     if (IsDebugMode)
-            //     {
-            //         Console.Draw(0, 0, $"Pos  : {Rover.Position}");
-            //         Console.Draw(0, 1, $"SPos : {Rover.GetScreenPos()}");
-            //         Console.Draw(0, 2, $"BB   : {Rover.BoundingBox}");
-            //     }
-            // }            
-        }
+        protected override void OnRender() { }
 
         public void RotateMap()
         {
@@ -170,8 +48,6 @@ namespace TerraForM
             if (CurrentMap >= Maps.Length) {
                 CurrentMap = 0;
             }
-
-            // StartNewGame();
         }
     }
 }
